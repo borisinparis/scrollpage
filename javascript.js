25488311682fc844a4f19075cdfcfc4acbe9b0df
borisinparis <mnkymgaa@gmail>
2024-12-03 13:15:20 +0800

    first page

---
+++ javascript.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+
+ScrollReveal().reveal('.main-title', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.sec-01 .image', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.text-box', { delay: 700, origin: 'right' });
+ScrollReveal().reveal('.media-icons i', { delay: 500, origin: 'bottom', interval: 200 });
+// ScrollReveal().reveal('.headline'); 
